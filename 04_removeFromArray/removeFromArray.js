da90ce5fc5ebc004fa42174018f09a29d5e97d07
Elizabeth Karr <e.karr93@icloud.com>
2023-02-04 17:07:46 -0600

    exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(array) {
+    
+    for (let i = 1; i < arguments.length; i++) {
 
+        if (!array.includes(arguments[i])) continue;
+
+        array.splice(array.indexOf(arguments[i]), 1);
+    }
+        
+
+    return array;
 };
 
 // Do not edit below this line
