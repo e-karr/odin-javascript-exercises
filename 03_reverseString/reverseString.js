8832856f65acbcd2fac18b87a1b0034f5d1399c2
Elizabeth Karr <e.karr93@icloud.com>
2023-02-04 16:23:58 -0600

    exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let reverseString = "";
 
+    for (let i = 0; i < string.length; i++) {
+        reverseString = string[i] + reverseString;
+    }
+
+    return reverseString;
 };
 
 // Do not edit below this line
