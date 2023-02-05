cc39c9f949dd20ba1357b589764e0f932e4cded2
Elizabeth Karr <e.karr93@icloud.com>
2023-02-04 19:14:14 -0600

    exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
+const sumAll = function(number1, number2) {
+    let sum = 0;
+    let smallerNumber = 0;
+    let largerNumber = 0;
 
+    if (number1 < 0 || number2 < 0 || !Number.isInteger(number1) || !Number.isInteger(number2)) {
+        return "ERROR";
+    }
+
+    if (number1 < number2) {
+        smallerNumber = number1;
+        largerNumber = number2;
+    } else {
+        smallerNumber = number2;
+        largerNumber = number1;
+    }
+
+    for (let i = smallerNumber; i <= largerNumber; i++) {
+        sum += i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
