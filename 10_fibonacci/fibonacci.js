015708e3f1bc848d8e7535a86612f22b7849b026
Elizabeth Karr <e.karr93@icloud.com>
2023-02-24 14:27:49 -0600

    complete fibonacci function

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,24 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    let fibonacci = [];
 
+    if (num < 0) {
+        return "OOPS";
+    }
+
+    if (num === 0) {
+        return 0;
+    }
+
+    for (let i = 0; i < num; i++) {
+        if (i === 0 || i === 1) {
+            fibonacci.push(1);
+        } else {
+            let nextNum = fibonacci[fibonacci.length-1] + fibonacci[fibonacci.length-2]
+            fibonacci.push(nextNum);
+        }
+    }
+
+    return fibonacci[num - 1];
 };
 
 // Do not edit below this line
