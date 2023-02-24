c83bad0e7a5005d54e6fddc668c733f19d0042c0
Elizabeth Karr <e.karr93@icloud.com>
2023-02-24 15:25:41 -0600

    complete find the oldest function

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,29 @@
-const findTheOldest = function() {
+const findTheOldest = function(array) {
+    const currentYear = (new Date()).getFullYear();
 
+    let oldest = array.sort(function(a, b) {
+        let aBirth = a.yearOfBirth;
+        let aDeath = 0;
+
+        let bBirth = b. yearOfBirth;
+        let bDeath = 0;
+
+        if (!a.yearOfDeath) {
+            aDeath= currentYear;
+        } else {
+            aDeath = a.yearOfDeath;
+        }
+
+        if (!b.yearOfDeath) {
+            bDeath = currentYear;
+        } else {
+            bDeath = b.yearOfDeath;
+        }
+
+        return (bDeath - bBirth) - (aDeath - aBirth);
+    });
+
+    return oldest[0];
 };
 
 // Do not edit below this line
