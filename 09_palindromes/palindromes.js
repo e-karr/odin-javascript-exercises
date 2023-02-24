1bc18e15d9019306a8c63282447b151fd9e829c9
Elizabeth Karr <e.karr93@icloud.com>
2023-02-24 14:08:55 -0600

    complete palindrome function

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
+const palindromes = function (phrase) {
+    let re = /[^A-Za-z0–9]/g;
 
+    let originalPhrase = phrase.toLowerCase().replace(re, '');
+
+    let reversedPhrase = originalPhrase.split('').reverse().join('');
+  
+    return originalPhrase === reversedPhrase;
 };
 
 // Do not edit below this line
